15af376e676622d170c30506eaffb1823faf7c0c
Matheus Souza <matheussouzam159@outlook.com>
2022-06-15 07:33:18 -0300

    migrations

---
+++ migrations/20220615101711-planetss.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  // inserindo no banco de dados
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("planets", {
+      id: {type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true},
+      name: {type: Sequelize.STRING, allowNull: false},
+      position: {type: Sequelize.INTEGER, allowNull: false},
+      createdAt:{type: Sequelize.DATE},
+      updatedAt: {type: Sequelize.DATE}
+    })
+  },
+  // deletando tabela
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("planets")
+  }
+};
