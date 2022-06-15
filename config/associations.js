c3d86390706097b05831cc487b76ada594a8041e
Matheus Souza <matheussouzam159@outlook.com>
2022-06-15 14:59:38 -0300

    association: has many

--- config/associations.js
+++ config/associations.js
@@ -3,6 +3,7 @@ const Satelite = require("../models/satellite")
 
 //Associação Has One or One to One
 
+/*
 Planet.hasOne(Satelite, {
     onDelete: "CASCADE",
     onUpdate: "CASCADE"
@@ -11,6 +12,17 @@ Satelite.belongsTo(Planet, {
     foreingKey: "planetId", 
     as: "planet"
 });
+*/
 
+//Associação Has Many or One to Many
+
+Planet.hasMany(Satelite, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+});
+Satelite.belongsTo(Planet, {
+    foreingKey: "planetId", 
+    as: "planet"
+});
 
 module.exports = {Planet, Satelite};
